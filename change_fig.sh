a3b967ce545db484f4ec3c1e14933bada220e15b
gadepall <gadepall@gmail.com>
2024-07-10 14:52:00 +0530

    Add existing files

--- change_fig.sh
+++ change_fig.sh
@@ -7,9 +7,10 @@ find . -type f -name "*.tex" | while IFS= read -r file; do
     # Use sed to modify figure environments
     # Change \begin{figure}[...]
     sed -i 's/\\begin{figure}\[.*\]/\\begin{figure}[H]/g' "$file"
+    sed -i 's/\\begin{table}\[.*\]/\\begin{table}[H]/g' "$file"
 
     # Change \includegraphics[...]{...}
-    sed -i 's/\\includegraphics\[\(.*\)\]{\([^}]*\)}/\\includegraphics[width=0.5\columnwidth]{\2}/g' "$file"
+    sed -i 's/\\includegraphics\[\(.*\)\]{\([^}]*\)}/\\includegraphics\[width=0.75\\columnwidth\]{\2}/g' "$file"
 done
 
 echo "Figure settings modified in all *.tex files."
